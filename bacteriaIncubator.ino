2fe4e9107f2e4c0333b2af15bfc8fba23f9d0adb
Pral2a <guillem@pral2a.com>
2014-10-03 19:16:15 +0200

    beta release

--- bacteriaIncubator.ino
+++ bacteriaIncubator.ino
@@ -10,7 +10,7 @@ float temp;
 
 void setup() {
   Serial.begin(9600); 
-  setMinMaxHeaterOn(3000, 8000);       // set the min and max intervals the heater can be ON in ms.
+  setMinMaxHeaterOn(3000, 10000);       // set the min and max intervals the heater can be ON in ms.
   updateTargetTemp(37.0);              // set the target temperature in ºC
   updateFanInterval(60000, 10000);     // set the time fan is OFF and and ON in ms. this is for homogenizing the heat. 
   setupSensors();
