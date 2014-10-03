3ebe75b9a3c286de9e694224d8adb333679b7d94
Pral2a <guillem@pral2a.com>
2014-10-03 19:15:31 +0200

    beta release

--- bacteriaIncubator.ino
+++ bacteriaIncubator.ino
@@ -1,16 +1,16 @@
 #include "PID_v1.h"
 #include "DHT.h"
 
-int heaterRelayPin = 12;
-int fanRelayPin = 11;
-int tempHumSensorPin = 2;
+const int heaterRelayPin = 12;
+const int fanRelayPin = 11;
+const int tempHumSensorPin = 2;
 
 float hum;
 float temp;
 
 void setup() {
   Serial.begin(9600); 
-  setMinMaxHeaterOn(2000, 7000);       // set the min and max intervals the heater can be ON in ms.
+  setMinMaxHeaterOn(3000, 8000);       // set the min and max intervals the heater can be ON in ms.
   updateTargetTemp(37.0);              // set the target temperature in ºC
   updateFanInterval(60000, 10000);     // set the time fan is OFF and and ON in ms. this is for homogenizing the heat. 
   setupSensors();
